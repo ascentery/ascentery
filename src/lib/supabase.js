3edbc75c58d05403e0ec0cd7a9d9f5817da2a3d1
Psyblade <p5yb14d3@gmail.com>
2026-06-16 01:52:12 +0800

    Fix supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-const SUPABASE_URL =
-  import.meta.env.VITE_SUPABASE_URL || "https://rlxzyxojexkwzumkfpir.supabase.co";
-const SUPABASE_ANON_KEY =
-  import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_mHXmPFVtqwKEjxTFL49_-Q_G5yB2_5w";
-);
+const SUPABASE_URL = "https://rlxzyxojexkwzumkfpir.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_mHXmPFVtqwKEjxTFL49_-Q_G5yB2_5w";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
